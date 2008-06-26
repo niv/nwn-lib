755170e0d3c1c640a6712323a54b2212618d11e2
Bernhard Stoeckner <elven@swordcoast.net>
2008-06-26 15:32:41 +0200

    Suspected to be fully-working Gff::Reader

--- gff.rb
+++ gff.rb
@@ -1,186 +1,302 @@
 class GffError < Exception; end
 module Gff; end
 
-class Gff::Element < Hash # < Struct.new(:type, :label, :value)
+module NWN
+  module Gff
+    class GffError < Exception; end
+
+    Types = {
+      0 => :byte,
+      1 => :char,
+      2 => :word,
+      3 => :short,
+      4 => :dword,
+      5 => :int,
+      6 => :dword64,
+      7 => :int64,
+      8 => :float,
+      9 => :double,
+      10 => :cexostr,
+      11 => :resref,
+      12 => :cexolocstr,
+      13 => :void,
+      14 => :struct,
+      15 => :list,
+    }.freeze
+
+    ComplexTypes = [6, 7, 9, 10, 11, 12, 13, 14, 15].freeze
+    SimpleTypes = (Types.keys - ComplexTypes)
+    SimpleTypes.freeze
+
+    Formats = {
+      :byte => "Cxxx",
+      :char => "Cxxx",
+      :word => 'Sxx',
+      :short => 'sxx',
+      :dword => 'I',
+      :int => 'i',
+      #:dword64 => 'Q',
+      #:int64 => '*q',
+      :float => 'f',
+      #:double => 'd',
+      #:cexostr => '*a*',
+      #:resref => '*a*',
+      #:void => '*HV/a*',
+    }.freeze
+  end
 end
-class Gff::Struct < Gff::Element
+
+class NWN::Gff::Gff
+  include NWN::Gff
+
+  attr_accessor :type
+  attr_accessor :version
+
+  def initialize hash, type, version = "V3.2"
+    @hash = hash
+    @type = type
+    @version = version
+  end
+
+  def root_struct
+    @hash
+  end
+
+  def [] k
+    h = @hash
+    k.split('/').each {|v|
+      vv = h[v]
+      h = vv
+      return vv
+    }
+  end
+
+  def []= k, v
+    # super
+  end
+
 end
 
-class Gff::Reader
-	attr_reader :hash
-	def initialize(bytes)
-		@bytes = bytes
-		read_all
-	end
+class NWN::Gff::Element
+  attr_accessor :label, :type, :value
+  attr_accessor :_str_ref
 
-	def read_all
-		offset = 0
+  def initialize
+    # @label, @type, @value = label, type, value
+  end
+end
 
-		@type, @version,
-		@struct_offset, @struct_count,
-		@field_offset, @field_count,
-		@label_offset, @label_count,
-		@field_data_offset, @field_data_count,
-		@field_indices_offset, @field_indices_count,
-		@list_indices_offset, @list_indices_count =
-			@bytes.unpack("a4a4 VV VV VV VV VV VV")
+class NWN::Gff::Struct < Hash
+  attr_accessor :struct_id
+  def initialize *a
+    @struct_id = 0
+    super
+  end
+end
 
-		raise GffError, "Unknown version #{@version}" unless
-			@version == "V3.2"
-		
-		raise GffError, "structOffset at wrong place, not a gff?" unless
-			@struct_offset == 56
+class NWN::Gff::CExoLocString < Struct.new(:language, :text)
+end
 
-		struct_len = @struct_count * 12
-		field_len  = @field_count * 16
-		label_len  = @label_count * 16
+class NWN::Gff::Reader
+  include NWN::Gff
 
-		@structs = @bytes[offset + @struct_offset, struct_len].unpack("V*")
-		@fields  = @bytes[offset + @field_offset, field_len].unpack("V*")
-		@labels  = @bytes[offset + @label_offset, label_len].unpack("A16" * @label_count)
-		@field_data = @bytes[offset + @field_data_offset, @field_data_count]
-		@field_indices = @bytes[offset + @field_indices_offset, @field_indices_count].unpack("V*")
-		@list_indices = @bytes[offset + @list_indices_offset, @list_indices_count].unpack("V*")
+  attr_reader :hash
+  attr_reader :gff
 
-		@gff = {}
-		all = read_struct 0
-		@hash = all
-	end
+  def initialize bytes
+    @bytes = bytes
+    read_all
+  end
 
-	# This iterates through a struct and reads all fields into a hash, which it returns.
-	def read_struct index
-		struct = Gff::Struct.new # gff = {}
+  # Reads +bytes+ as gff data and returns a Gff:Gff object
+  def self.read bytes
+    self.new(bytes).gff
+  end
 
-		type = @structs[index * 3]
-		data_or_offset = @structs[index * 3 + 1]
-		count = @structs[index * 3 + 2]
+  private
 
-		raise GffError, "Struct index #{index} outside of struct_array" if
-			index * 3 + 3 > @structs.size + 1
+  def read_all
+    type, version,
+    struct_offset, struct_count,
+    field_offset, field_count,
+    label_offset, label_count,
+    field_data_offset, field_data_count,
+    field_indices_offset, field_indices_count,
+    list_indices_offset, list_indices_count =
+      @bytes.unpack("a4a4 VV VV VV VV VV VV")
 
-		if count == 1
-			lbl, vl = * read_field(data_or_offset)
-			struct[lbl] = vl
-		else
-			return 1 if count == 0
-			raise GffError, "Struct index not divisable by 4" if
-				data_or_offset % 4 != 0
-			data_or_offset /= 4
-			for i in data_or_offset...(data_or_offset+count)
-				lbl, vl = * read_field(@field_indices[i])
-				struct[lbl] = vl
-			end
-		end
+    raise GffError, "Unknown version #{@version}; not a gff?" unless
+      version == "V3.2"
 
-		struct.merge! :type => type
-		struct
-	end
+    raise GffError, "struct offset at wrong place, not a gff?" unless
+      struct_offset == 56
 
-	# Reads the field at +index+ and returns [label_name, Gff::Element]
-	def read_field index
-		gff = {}
+    struct_len = struct_count * 12
+    field_len  = field_count * 16
+    label_len  = label_count * 16
 
-		field = Gff::Element.new
+    @structs = @bytes[struct_offset, struct_len].unpack("V*")
+    @fields  = @bytes[field_offset, field_len].unpack("V*")
+    @labels  = @bytes[label_offset, label_len].unpack("A16" * label_count)
+    @field_data = @bytes[field_data_offset, field_data_count]
+    @field_indices = @bytes[field_indices_offset, field_indices_count].unpack("V*")
+    @list_indices = @bytes[list_indices_offset, list_indices_count].unpack("V*")
+    # puts "FieldDataOffset = #{field_data_offset}, Count = #{field_data_count}"
+    @hash = read_struct 0
+    @gff = Gff.new(@hash, type, version)
+  end
 
-		index *= 3
-		type = @fields[index]
-		label_index = @fields[index + 1]
-		data_or_offset = @fields[index + 2]
-		
-		raise GffError, "Label index #{label_index} outside of label array" if
-			label_index > @labels.size
-		
-		label = @labels[label_index]
+  # This iterates through a struct and reads all fields into a hash, which it returns.
+  def read_struct index
+    struct = Gff::Struct.new
 
-		if @type == 14 || @type == 15
-			raise "UnsupportedDtpyE"
-		else
-		end
-		value = case type
-			when 0 #byte
-				data_or_offset & 0xff
-			when 1 #char
-				data_or_offset & 0xff
-			when 2 #word
-				data_or_offset & 0xffff
-			when 3 #short
-				[(data_or_offset & 0xffff)].pack("S").unpack("s")[0]
-			when 4 #dword
-				data_or_offset
-			when 5 #int
-				[data_or_offset].pack("I").unpack("i")[0]
-			when 8 #float
-				[data_or_offset].pack("V").unpack("f")[0]
-			when 14 #struct
-				read_struct data_or_offset
-				# raise "substruct: #{str.inspect}"
-				# read_struct 
-			when 15 #list
-				list = []
+    type = @structs[index * 3]
+    data_or_offset = @structs[index * 3 + 1]
+    count = @structs[index * 3 + 2]
 
-				raise GffError, "List index not divisable by 4" unless
-					data_or_offset % 4 == 0
+    raise GffError, "struct index #{index} outside of struct_array" if
+      index * 3 + 3 > @structs.size + 1
 
-				data_or_offset /= 4
-				
-				raise GffError, "List index outside list indices" if
-					data_or_offset > @list_indices.size
+    if count == 1
+      lbl, vl = * read_field(data_or_offset)
+      struct[lbl] = vl
+    else
+      return 1 if count == 0
+      raise GffError, "struct index not divisable by 4" if
+        data_or_offset % 4 != 0
+      data_or_offset /= 4
+      for i in data_or_offset...(data_or_offset+count)
+        lbl, vl = * read_field(@field_indices[i])
+        struct[lbl] = vl
+      end
+    end
 
-				count = @list_indices[data_or_offset]
+    struct.struct_id = type
+    struct
+  end
 
-				raise GffError, "List index overflow the list indices array" if
-					data_or_offset + count > @list_indices.size
+  # Reads the field at +index+ and returns [label_name, Gff::Element]
+  def read_field index
+    gff = {}
 
-				data_or_offset += 1
-				
-				for i in data_or_offset...(data_or_offset + count)
-					list << read_struct(@list_indices[i])
-				end
+    field = Element.new
 
-				list
-			else
-				raise GffError, "Field data offset #{data_or_offset} outside of field data block" if
-					data_or_offset > @field_data.size
+    index *= 3
+    type = @fields[index]
+    label_index = @fields[index + 1]
+    data_or_offset = @fields[index + 2]
+    # puts "Reading field #{index}"
+    # puts "Label_index = #{label_index}, label = #{@labels[label_index]}"
+    # puts "type = #{type}, data_or_offset = #{data_or_offset}"
+    raise GffError, "Label index #{label_index} outside of label array" if
+      label_index > @labels.size
 
-				inner_value = case type
-					when 6 #dword64
-						len = 8
-						v1, v2 = @field_data[data_or_offset, len].unpack("II")
-						v1 * (2**32) + v2
-					when 7 #int64
-						len = 8
-						@field_data[data_or_offset, len].unpack("q")[0]
-					when 9 #double
-						len = 8
-						@field_data[data_or_offset, len].unpack("d")[0]
-					when 10 #cexostring
-						len = @field_data[data_or_offset, 4].unpack("V")[0]
-						@field_data[data_or_offset + 1, len]
-					when 11 #resref
-						len = @field_data[data_or_offset, 1].unpack("C")[0]
-						@field_data[data_or_offset + 1, len]
-					when 12 #cexolocstring
-						size, str_ref, str_count =
-							@field_data[data_or_offset, 12].unpack("VVV")
-						field['_str_ref'] = str_ref
-						str = @field_data[data_or_offset + 12, size - 8].
-							unpack("VV/a" * str_count)
-						len = size + 4
-						str
-					when 13 #void
-						len = @field_data[data_or_offset, 4].unpack("V")
-						void = @field_data[data_or_offset + 4, len].unpack("H*")
-						raise "void: #{void.inspect}"
-				end
+    raise GffError, "Field data offset #{data_or_offset} outside of field data block (#{@field_data.size})" if
+      ComplexTypes.index(type) && data_or_offset > @field_data.size
 
-				raise GffError, "Field data overflows from the field data block area\
-					offset = #{data_or_offset + len}, len = #{@field_data.size}" if
-					data_or_offset + len > @field_data.size
+    raise GffError, "Unknown field type #{type}." unless Types[type]
+    type = Types[type]
 
-				inner_value
-		end
-		field.merge! :type => type, :value => value
+    label = @labels[label_index]
 
-		[label, field]  #::Gff::Element.new(type,label,value)
-	end
+    value = case type
+      when :byte, :char
+        data_or_offset & 0xff
+
+      when :word
+        data_or_offset & 0xffff
+
+      when :short
+        [(data_or_offset & 0xffff)].pack("S").unpack("s")[0]
+
+      when :dword
+        data_or_offset
+
+      when :int
+        [data_or_offset].pack("I").unpack("i")[0]
+
+      when :float
+        [data_or_offset].pack("V").unpack("f")[0]
+
+      when :dword64
+        len = 8
+        v1, v2 = @field_data[data_or_offset, len].unpack("II")
+        v1 * (2**32) + v2
+
+      when :int64
+        len = 8
+        @field_data[data_or_offset, len].unpack("q")[0]
+
+      when :double
+        len = 8
+        @field_data[data_or_offset, len].unpack("d")[0]
+
+      when :cexostr
+        len = @field_data[data_or_offset, 4].unpack("V")[0]
+        @field_data[data_or_offset + 4, len]
+
+      when :resref
+        len = @field_data[data_or_offset, 1].unpack("C")[0]
+        @field_data[data_or_offset + 1, len]
+
+      when :cexolocstr
+        total_size, str_ref, str_count =
+          @field_data[data_or_offset, 12].unpack("VVV")
+        all = @field_data[data_or_offset + 12, total_size]
+        field._str_ref = str_ref
+
+        r = []
+        str_count.times {
+          id, len = all.unpack("VV")
+          str = all[8, len].unpack("a*")[0]
+          all = all[(8 + len)..-1]
+          r << Gff::CExoLocString.new(id, str)
+        }
+        len = total_size + 4
+        r
+
+      when :void
+        len = @field_data[data_or_offset, 4].unpack("V")
+        void = @field_data[data_or_offset + 4, len].unpack("H*")
+        raise "void: #{void.inspect}"
+
+      when :struct
+        read_struct data_or_offset
+
+      when :list
+        list = []
+
+        raise GffError, "List index not divisable by 4" unless
+          data_or_offset % 4 == 0
+
+        data_or_offset /= 4
+
+        raise GffError, "List index outside list indices" if
+          data_or_offset > @list_indices.size
+
+        count = @list_indices[data_or_offset]
+
+        raise GffError, "List index overflow the list indices array" if
+          data_or_offset + count > @list_indices.size
+
+        data_or_offset += 1
+
+        for i in data_or_offset...(data_or_offset + count)
+          list << read_struct(@list_indices[i])
+        end
+
+        list
+
+    end
+
+    raise GffError, "Field data overflows from the field data block area\
+      offset = #{data_or_offset + len}, len = #{@field_data.size}" if
+      len && data_or_offset + len > @field_data.size
+
+    field.label = label
+    field.type = type
+    field.value = value
+
+    [label, field]  #::Gff::Element.new(type,label,value)
+  end
 end
