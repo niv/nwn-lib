cd69a072828f3ae5bb6d376990fd7864be86273f
Bernhard Stoeckner <elven@swordcoast.net>
2011-06-02 21:03:07 +0200

    fix broken rspecs for :dword64, :int64

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,10 +21,10 @@ GffFieldValidations = {
   :resref => [["", "a" * 16], ["a" * 17]],
   :double => [[0.0], ["x"]],
   :dword => [[0, 0xffffffff], [-1, 0xffffffff + 1]],
-  :dword64 => [[0, 0xffffffffffff], [-1, 0xffffffffffff + 1]],
+  :dword64 => [[0, 0xffffffffffffffff], [-1, 0xffffffffffffffff + 1]],
   :float => [[0.0], ["x"]],
   :int => [[-0x80000000, 0x7fffffff], [0x80000001, 0x7fffffff + 1]],
-  :int64 => [[-0x800000000000, 0x7fffffffffff], [0x800000000001, 0x7fffffffffff + 1]],
+  :int64 => [[-0x8000000000000000, 0x7fffffffffffffff], [-0x8000000000000000 - 1, 0x7fffffffffffffff + 1]],
   :short => [[-0x8000, 0x7fff], [-0x8001, 0x7fff + 1]],
   :word => [[0, 0xffff], [-1, 0xffff + 1]],
 }.freeze
